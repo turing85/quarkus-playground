d84ed17049de3ca85c9ac389e67ca967f713276e
Marco Bungart <marco.bungart@consol.de>
2019-03-29 09:14:59 +0100

    - renamed `UserGroupApp` to `UserApp`

--- src/main/java/de/consol/dus/quarkus/resources/UserApp.java
+++ src/main/java/de/consol/dus/quarkus/resources/UserApp.java
@@ -7,14 +7,14 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/")
-public class UserGroupApp extends Application {
+public class UserApp extends Application {
 
   private static final Set<Class<?>> classes = new HashSet<>(
       Arrays.asList(UserResource.class));
 
   @Override
   public Set<Class<?>> getClasses() {
-    return UserGroupApp.classes;
+    return UserApp.classes;
   }
 
 }
